c6f9867132b879c46ab8dc358e64a08b0510a767
TutorialFollower <95521018+TutorialFollower@users.noreply.github.com>
2022-02-11 08:04:35 -0500

    Create Sus_Lang-KeyWords.h
    This is a very sussy language, so use at own risk.  
    include it with #include "Sus_Lang-KeyWords.h" 
    Sus Lang all rights reserved. Ⓒ

---
+++ Sus_Lang-KeyWords.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Data Types
+typedef int INT;
+typedef double INT32;
+typedef long INT64;
+typedef char CHAR;
+typedef string STRING;
+
+//Function Placeholder
+string whileExp;
+string ifExp;
+string funcationParams;
+
+//It's bad practice use macros but I dont know any other way lol
+#define WHILE(whileExp) while(whileExp)
+#define REPEAT(i,N) for(int (i) = 0; (i) < (N); ++(i))
+#define IF(ifExp) if(ifExp)
+
+//Print message to console
+int REPORT(string message) {
+	cout << message;
+	return 0;
+}
